084797131181031e11c576f6be3cfa62a0ecf47b
bradenlimb <71336960+bradenlimb@users.noreply.github.com>
2022-05-01 15:48:13 -0600

    Initial Files

---
+++ all_file_copier.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 14:13:39 2022
+
+@author: Braden Limb
+"""
+
+#%% Import Modules
+from IPython import get_ipython
+get_ipython().run_line_magic('reset','-sf')
+
+# import pandas as pd
+# import sys
+import datetime
+begin_time = datetime.datetime.now()
+import shutil
+import os
+from tqdm import tqdm
+
+import pytz
+from win32com.propsys import propsys, pscon
+
+def make_dir(path):
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+    
+from PIL import Image
+def get_date_taken(path):
+    return Image.open(path)._getexif()[36867]
+
+import exifread
+def exif_read(directoryInput,filename):
+        with open("%s/%s" % (directoryInput, filename), 'rb') as image: # file path and name
+            exif = exifread.process_file(image)
+            dt = str(exif['EXIF DateTimeOriginal'])  # might be different
+            return dt
+
+
+#%% Base Location
+
+camera = 'R6'
+camera = 'iPhone 11'
+
+source_root = r'C:\Users\Braden Limb\Downloads\iCloud Photos (1)\iCloud Photos'
+target_root = r'F:\Pictures'
+
+not_copied = []
+move_files = True
+if move_files:
+    filenames = [ item for item in os.listdir(source_root) if os.path.isfile(os.path.join(source_root, item)) ]
+    filenames = [ x for x in filenames if "._" not in x ]
+    # filenames = [ x for x in filenames if "IMG_" in x ]
+    # filenames  = ['IMG_9514.HEIC']
+    # filename = 'IMG_9514.HEIC'
+    for filename in tqdm(filenames):
+        
+        filepath = f'{source_root}\{filename}'
+        
+        try:
+            dt = get_date_taken(filepath)
+        except:
+            try:
+                dt = exif_read(source_root,filename)
+            except:
+                try:
+                    properties = propsys.SHGetPropertyStoreFromParsingName(filepath)
+                    dt = properties.GetValue(pscon.PKEY_Media_DateEncoded).GetValue()
+                except:
+                    try:
+                       dt = os.path.getctime(filepath) 
+                    except: 
+                        not_copied.append(filename)
+                        continue
+        
+        if not isinstance(dt, datetime.datetime):
+            
+            if dt == None:
+                not_copied.append(filename)
+                continue
+            
+            # In Python 2, PyWin32 returns a custom time type instead of
+            # using a datetime subclass. It has a Format method for strftime
+            # style formatting, but let's just convert it to datetime:
+                
+            try:
+                dt = datetime.datetime.strptime(dt, '%Y:%m:%d %H:%M:%S')
+            except:
+                dt = datetime.datetime.fromtimestamp(int(dt))
+            dt = dt.replace(tzinfo=pytz.timezone('UTC'))
+        dt_local = dt.astimezone(pytz.timezone('America/Denver'))
+        
+        target_path = f'{target_root}\{dt_local.strftime("%Y")}'
+        make_dir(target_path)
+        target_path = f'{target_path}\{dt_local.strftime("%m")} {dt_local.strftime("%B")}'
+        make_dir(target_path)
+        target_path = f'{target_path}\{camera}'
+        make_dir(target_path)
+
+        target_file = f'{target_path}\{filename}'
+        shutil.copyfile(filepath, target_file)
+
+
+#%% End of Code
+execute_time = datetime.datetime.now() - begin_time
+print('')
+print('Code execution time: ', execute_time)
---
+++ video_diaries_copier.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 13:18:16 2022
+
+@author: bradenlimb
+"""
+#%% Import Modules
+from IPython import get_ipython
+get_ipython().run_line_magic('reset','-sf')
+
+# import pandas as pd
+# import sys
+import datetime
+begin_time = datetime.datetime.now()
+import shutil
+import os
+from tqdm import tqdm
+
+import pytz
+from win32com.propsys import propsys, pscon
+
+#%% Base Location
+
+source_root = r'C:\Users\Braden Limb\Downloads\iCloud Photos (3)\iCloud Photos'
+target_root = r'E:\Video Diaries'
+
+rename_existing_files = False
+if rename_existing_files:
+    filenames = [ item for item in os.listdir(target_root) if os.path.isfile(os.path.join(target_root, item)) ]
+    filenames = [ x for x in filenames if "._" not in x ]
+    filenames = [ x for x in filenames if "IMG_" in x ]
+    #filenames  = [filenames[1]]
+    for filename in tqdm(filenames):
+        
+        filepath = f'{target_root}\{filename}'
+        properties = propsys.SHGetPropertyStoreFromParsingName(filepath)
+        dt = properties.GetValue(pscon.PKEY_Media_DateEncoded).GetValue()
+        
+        if not isinstance(dt, datetime.datetime):
+            # In Python 2, PyWin32 returns a custom time type instead of
+            # using a datetime subclass. It has a Format method for strftime
+            # style formatting, but let's just convert it to datetime:
+            dt = datetime.datetime.fromtimestamp(int(dt))
+            dt = dt.replace(tzinfo=pytz.timezone('UTC'))
+        dt_local = dt.astimezone(pytz.timezone('America/Denver'))
+        
+        dt_local_str = dt_local.strftime('%Y-%m-%d')
+        new_filename = f'{dt_local_str} Video Diary{filename[-4:]}'
+        target_file = f'{target_root}\{new_filename}'
+        shutil.copyfile(filepath, target_file)
+
+move_files = True
+if move_files:
+    filenames = [ item for item in os.listdir(source_root) if os.path.isfile(os.path.join(source_root, item)) ]
+    filenames = [ x for x in filenames if "._" not in x ]
+    filenames = [ x for x in filenames if "IMG_" in x ]
+    # filenames  = [filenames[1]]
+    for filename in tqdm(filenames):
+        
+        filepath = f'{source_root}\{filename}'
+        properties = propsys.SHGetPropertyStoreFromParsingName(filepath)
+        dt = properties.GetValue(pscon.PKEY_Media_DateEncoded).GetValue()
+        
+        if not isinstance(dt, datetime.datetime):
+            # In Python 2, PyWin32 returns a custom time type instead of
+            # using a datetime subclass. It has a Format method for strftime
+            # style formatting, but let's just convert it to datetime:
+            dt = datetime.datetime.fromtimestamp(int(dt))
+            dt = dt.replace(tzinfo=pytz.timezone('UTC'))
+        dt_local = dt.astimezone(pytz.timezone('America/Denver'))
+        
+        dt_local_str = dt_local.strftime('%Y-%m-%d')
+        new_filename = f'{dt_local_str} Video Diary{filename[-4:]}'
+        target_file = f'{target_root}\{new_filename}'
+        shutil.copyfile(filepath, target_file)
+
+#%% End of Code
+execute_time = datetime.datetime.now() - begin_time
+print('')
+print('Code execution time: ', execute_time)
